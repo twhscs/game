d28d9a82c3e6bb71bdb967d295dd01338634ef61
Robert Maes <remilmaes@gmail.com>
2015-01-31 04:43:32 -0600

    Add basic animation

--- src/main/java/io/github/twhscs/game/Player.java
+++ src/main/java/io/github/twhscs/game/Player.java
@@ -13,6 +13,7 @@ class Player implements Drawable {
     private Position position;
     private Direction direction;
     private Map map;
+    private int animationFrame;
 
     Player(Texture playerTexture, View GAME_VIEW, Vector2i SPRITE_SIZE, int TILE_SIZE) {
         this.SPRITE = new Sprite(playerTexture);
@@ -21,6 +22,7 @@ class Player implements Drawable {
         this.TILE_SIZE = TILE_SIZE;
         position = new Position(0.0f, 0.0f);
         direction = Direction.NORTH;
+        animationFrame = 0;
         updateSprite();
     }
 
@@ -44,38 +46,48 @@ class Player implements Drawable {
         float positionX = position.getX() * 32 - ((SPRITE_SIZE.x - TILE_SIZE) / 2);
         float positionY = position.getY() * 32 - (SPRITE_SIZE.y - TILE_SIZE);
         SPRITE.setPosition(positionX, positionY);
-        // 32 * (spritex - tile), 32 * spritey - tile
         SPRITE.setTextureRect(getTextureRect());
         GAME_VIEW.setCenter(SPRITE.getPosition());
-        System.out.println(SPRITE.getLocalBounds());
     }
 
     public void move(Direction direction) {
-        Position newPosition = position.getRelativePosition(direction);
-        if (map.isValidPosition(newPosition.getPosition())) {
-            position = newPosition;
-            this.direction = direction;
-            updateSprite();
+        if (animationFrame == 0) {
+            Position newPosition = position.getRelativePosition(direction);
+            if (map.isValidPosition(newPosition.getPosition())) {
+                position = newPosition;
+                this.direction = direction;
+                animationFrame++;
+                updateSprite();
+            }
         }
     }
 
     private IntRect getTextureRect() {
         switch (direction) {
             case NORTH:
-                return new IntRect(0, 3 * SPRITE_SIZE.y, SPRITE_SIZE.x, SPRITE_SIZE.y);
+                return new IntRect(animationFrame * SPRITE_SIZE.x, 3 * SPRITE_SIZE.y, SPRITE_SIZE.x, SPRITE_SIZE.y);
             case SOUTH:
-                return new IntRect(0, 0, SPRITE_SIZE.x, SPRITE_SIZE.y);
+                return new IntRect(animationFrame * SPRITE_SIZE.x, 0, SPRITE_SIZE.x, SPRITE_SIZE.y);
             case WEST:
-                return new IntRect(0, SPRITE_SIZE.y, SPRITE_SIZE.x, SPRITE_SIZE.y);
+                return new IntRect(animationFrame * SPRITE_SIZE.x, SPRITE_SIZE.y, SPRITE_SIZE.x, SPRITE_SIZE.y);
             case EAST:
-                return new IntRect(0, 2 * SPRITE_SIZE.y, SPRITE_SIZE.x, SPRITE_SIZE.y);
+                return new IntRect(animationFrame * SPRITE_SIZE.x, 2 * SPRITE_SIZE.y, SPRITE_SIZE.x, SPRITE_SIZE.y);
             default:
                 return new IntRect(0, 0, 0, 0);
         }
     }
 
     public void update() {
-
+        if (animationFrame > 0) {
+            System.out.println(getTextureRect());
+            if (animationFrame > 3) {
+                animationFrame = 0;
+                updateSprite();
+            } else {
+                animationFrame++;
+                updateSprite();
+            }
+        }
     }
 
     @Override
