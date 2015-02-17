2353f84cbc5afef2f1c01b8fa8c4c1b1b3f06b28
Kyle Lee <bassicsmusic@gmail.com>
2015-02-17 09:36:42 -0600

    Create SoundManager class

---
+++ src/main/java/io/github/twhscs/game/util/SoundManager.java
@@ -0,0 +1,11 @@
+package io.github.twhscs.game.util;
+
+import org.jsfml.audio.Sound;
+import org.jsfml.audio.SoundBuffer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SoundManager {
+
+}
