7cd7d3d9f8d5a2fae414ab2168f97ab0bb9415b6
Kyle Lee <bassicsmusic@gmail.com>
2015-02-17 09:37:07 -0600

    Add functionality to SoundManager

--- src/main/java/io/github/twhscs/game/util/SoundManager.java
+++ src/main/java/io/github/twhscs/game/util/SoundManager.java
@@ -7,5 +7,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class SoundManager {
-
+    private final Map<String, Sound> SOUND_MAP;
+    public SoundManager() {
+        SOUND_MAP = new HashMap<String, Sound>();
+    }
+    public void addSound(String name, SoundBuffer soundBuffer) {
+        Sound newSound = new Sound();
+        newSound.setBuffer(soundBuffer);
+        SOUND_MAP.put(name, newSound);
+    }
+    public Sound getSound(String name) {
+        return SOUND_MAP.get(name);
+    }
 }
