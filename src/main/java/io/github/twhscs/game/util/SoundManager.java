3623a50b190d5923926142287d10bfe7111c6a08
Kyle Lee <bassicsmusic@gmail.com>
2015-02-17 10:39:53 -0600

    Add more functionality to SoundManager

--- src/main/java/io/github/twhscs/game/util/SoundManager.java
+++ src/main/java/io/github/twhscs/game/util/SoundManager.java
@@ -19,4 +19,16 @@ public class SoundManager {
     public Sound getSound(String name) {
         return SOUND_MAP.get(name);
     }
+    public void playSound(String name) {
+        SOUND_MAP.get(name).play();
+    }
+    public void pauseSound(String name) {
+        SOUND_MAP.get(name).pause();
+    }
+    public void stopSound(String name) {
+        SOUND_MAP.get(name).stop();
+    }
+    public void setLoop(String name, boolean toggle) {
+        SOUND_MAP.get(name).setLoop(toggle);
+    }
 }
